c717b39d6419bd62086b65c7cef77a8981f34179
Yi Tseng <a86487817@gmail.com>
2016-04-18 00:15:41 +0800

    Remove unused debug message

--- fwd_bgp.py
+++ fwd_bgp.py
@@ -156,6 +156,4 @@ class FwdBGP(app_manager.RyuApp):
 
         inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
         mod = parser.OFPFlowMod(datapath=datapath, priority=priority, match=match, instructions=inst)
-        self.logger.info(mod)
         datapath.send_msg(mod)
-
