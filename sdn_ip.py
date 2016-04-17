bec4456cf7f09e6999e8a1deb259a8cca3451d14
Takeshi <a86487817@gmail.com>
2016-04-17 15:08:30 +0800

    Fix typo

--- sdn_ip.py
+++ sdn_ip.py
@@ -2,7 +2,7 @@ from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
-from ryu.ofproto import ofproto_v1_0, ofproto_v3_0
+from ryu.ofproto import ofproto_v1_0, ofproto_v1_3
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
