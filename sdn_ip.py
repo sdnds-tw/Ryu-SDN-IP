07ddb1f2f5410c13dadce21318118ad30c95a3b6
Yi Tseng <a86487817@gmail.com>
2016-05-09 04:02:19 +0800

    Add `install_internal_host_path` function

--- sdn_ip.py
+++ sdn_ip.py
@@ -120,3 +120,107 @@ class SDNIP(app_manager.RyuApp):
                                          pre_actions)
 
         self.hop_db.install_prefix(prefix)
+
+    def install_internal_host_path(self, ip):
+        host = self.get_host(ip)
+
+        if host is None:
+            return
+
+        for dp in self.fwd.get_all_datapaths():
+            from_dpid = dp.id
+            host_match =\
+            dp.ofproto_parser.OFPMatch(ipv4_dst=ip,
+                                       eth_type=2048)
+            pre_actions = [
+                dp.ofproto_parser.OFPActionSetField(eth_dst=host.mac)
+                ]
+
+            self.fwd.setup_shortest_path(from_dpid,
+                                         host.port.dpid,
+                                         host.port.port_no,
+                                         host_match,
+                                         pre_actions)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def internal_host_route_handler(self, ev):
+        '''
+        Handle internal network host routing
+        '''
+        msg = ev.msg
+        dp = msg.datapath
+        dpid = dp.id
+        ofproto = dp.ofproto
+
+        pkt = packet.Packet(msg.data)
+        ipv4_header = pkt.get_protocol(ipv4.ipv4)
+
+        if ipv4_header == None:
+            return
+
+        src_ip = ipv4_header.src
+        dst_ip = ipv4_header.dst
+
+        if not self.cfg_mgr.is_internal_host(dst_ip):
+            return
+
+        install_internal_host_path(dst_ip)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def internal_host_arp_proxy(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        in_port = msg.match['in_port']
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocol(ethernet.ethernet)
+
+        if eth.ethertype != ether_types.ETH_TYPE_ARP:
+            # ignore non-arp packet
+            return
+
+        arp_header = pkt.get_protocol(arp.arp)
+        src_ip = arp_header.src_ip
+        src_mac = arp_header.src_mac
+        dst_ip = arp_header.dst_ip
+        if arp_header.opcode != arp.ARP_REQUEST:
+            return
+
+        # Use first bgp speaker as gateway
+        # reply first bgp speaker's mac address
+        # TODO: TBD
+        speaker_ids = self.cfg_mgr.get_all_speaker_id()
+
+        if not speaker_ids:
+            # no speaker to use
+            return
+
+        dst_mac = self.cfg_mgr.get_speaker_mac(speaker_ids[0])
+        # send arp request to host
+        actions = [parser.OFPActionOutput(in_port)]
+        arp_reply = packet.Packet()
+        arp_reply.add_protocol(
+            ethernet.ethernet(
+                ethertype=ether_types.ETH_TYPE_ARP,
+                src=dst_mac,
+                dst=src_mac
+            )
+        )
+        arp_reply.add_protocol(
+            arp.arp(
+                opcode=arp.ARP_REPLY,
+                src_ip=dst_ip,
+                src_mac=dst_mac,
+                dst_ip=src_ip,
+                dst_mac=src_mac
+            )
+        )
+        arp_reply.serialize()
+
+        out = parser.OFPPacketOut(
+            datapath=datapath,
+            buffer_id=ofproto.OFP_NO_BUFFER,
+            in_port=ofproto.OFPP_CONTROLLER,
+            actions=actions, data=arp_reply.data)
+        datapath.send_msg(out)
